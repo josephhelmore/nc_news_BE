eb45986ada79e8e3d11322203a8531157f1700c5
admin <admin@admins-MacBook-Air.local>
2025-10-27 16:30:11 +0000

    homepage updates

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -73,13 +73,14 @@ const deleteCommentById = (req, res, next) => {
     .catch(next);
 };
 const APIHome = (req, res) => {
-  res.status(200).send({ message: `Thank you for visiting my first, small back-end project.\n Since you have already added /api to the url - Here are some more end-points for you to try; \n 
-    /topics \n
-    /articles \n
-    /users \n
-    /articles/:article_id \n
-    /articles/:article_id/comments \n
-    You can also POST comments to /articles/:article_id/comments and PATCH votes to /articles/:article_id \n
+  res.status(200).type('text/plain').send({ message: `Thank you for visiting my first, small back-end project.
+    Since you have already added /api to the url - Here are some more end-points for you to try;
+    /topics 
+    /articles 
+    /users 
+    /articles/:article_id 
+    /articles/:article_id/comments 
+    You can also POST comments to /articles/:article_id/comments and PATCH votes to /articles/:article_id 
     Enjoy :)` });
 }
 
