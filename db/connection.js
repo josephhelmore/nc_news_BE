6b532afce0213957acf428a93f0cb27c4b861258
admin <admin@admins-MacBook-Air.local>
2025-10-27 15:08:34 +0000

    hosting updating

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,20 @@
-
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
-}
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const config = {};
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-module.exports = new Pool(config);
 
-module.exports = db;
+
+module.exports = new Pool(config);
