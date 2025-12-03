9e5ac75a837ff2c9cc7bf607c4b944d02ed7a8b9
admin <admin@admins-MacBook-Air.local>
2025-12-03 17:35:46 +0000

    converting to async await

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use((err, req, res, next) => {
   }
 
   console.log(err);
+  
   res.status(500).send({ message: "Internal Server Error" });
 });
 
--- controllers/get-controllers.js
+++ controllers/get-controllers.js
@@ -6,52 +6,66 @@ const {
   fetchArticleData,
   fetchArticleComments,
 } = require("../models/fetch-models");
-const {isTopic} = require("./controller-error-handling")
+const { isTopic, validId } = require("./controller-error-handling");
 
-const getTopics = (req, res) => {
-  fetchTopics().then((topics) => {
+const getTopics = async (req, res) => {
+  try {
+    const topics =  await fetchTopics();
     res.status(200).send({ topics });
-  });
-};
-const getArticles = (req, res, next) => {
-  const { sort_by, order, topic } = req.query;
-
- if (topic) {
-    return isTopic(topic)
-      .then(() => fetchArticles(sort_by, order, topic))
-      .then((articles) => res.status(200).send({ articles }))
-      .catch(next);
+  } catch (err) {
+    next(err);
   }
-
-  fetchArticles(sort_by, order, topic)
-    .then((articles) => {
-      res.status(200).send({ articles });
-    })
-    .catch(next);
 };
-const getUsers = (req, res) => {
-  fetchUsers().then((users) => {
-    res.status(200).send({ users });
-  });
-};
-const getArticleData = (req, res, next) => {
-  const { article_id } = req.params;
+const getArticles = async (req, res, next) => {
+  try {
+    const { sort_by, order, topic } = req.query;
 
-  //validate article_id
+    if (topic) {
+      await isTopic(topic);
+    }
 
-  fetchArticleData(article_id)
-    .then((data) => {
-      res.status(200).send({ article: data });
-    })
-    .catch(next);
+    const articles = await fetchArticles(sort_by, order, topic);
+    res.status(200).send({ articles });
+  } catch (err) {
+    next(err);
+  }
 };
-const getArticleComments = (req, res, next) => {
-  const { article_id } = req.params;
-  fetchArticleComments(article_id)
-    .then((data) => {
-      res.status(200).send({ comments: data });
-    })
-    .catch(next);
+const getUsers = async (req, res) => {
+  try {
+    fetchUsers().then((users) => {
+      res.status(200).send({ users });
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+const getArticleData = async (req, res, next) => {
+  try {
+    const { article_id } = req.params;
+
+    await validId(article_id);
+
+    const data = await fetchArticleData(article_id);
+
+    if (!data) {
+      return res.status(404).send({ msg: "Article not found" });
+    }
+
+    res.status(200).send({ article: data });
+  } catch (err) {
+    next(err);
+  }
+};
+const getArticleComments = async (req, res, next) => {
+  try {
+    const { article_id } = req.params;
+
+    const data = await fetchArticleComments(article_id);
+
+    res.status(200).send({ comments: data });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
